9fe8349f8f096e33554e8d684e6e2915651e5870
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-02-03 14:53:08 +0300

    izmenen

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import { brainEven } from '../src/cli2.js';
-
-brainEven();
-
-
+import { playEven } from '../src/games/even.js';
+import { playGame } from '../src/index.js';
+const question = 'Answer "yes" if the number is even, otherwise answer "no".';
+playGame(playEven, question);
