2c9d8e07eed9d9cedd85a37cc6918189e04d58d4
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-02-08 02:09:39 +0300

    linter

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,7 +2,9 @@ const playGcd = () => {
   const num1 = Math.floor(Math.random() * 100) + 1;
   const num2 = Math.floor(Math.random() * 100) + 1;
   console.log(`Question: ${num1} ${num2}`);
-  const findGCD = (num1, num2) => {
+  const findGCD = (a, b) => {
+    let a = num1;
+    let b = num2;
     while (num1 !== num2) {
       if (num1 > num2) {
         num1 -= num2;
