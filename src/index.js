a46168fca6fc41d1ba14a0319b597330c2188ab8
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-02-08 00:06:01 +0300

    linter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import readlineSync from 'readline-sync';
 
 export const playGame = (game, question) => {
-
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log(`${ question }`);
+  console.log(`${question}`);
 
   let correctAnswers = 0;
   const maxQuestions = 3;
   let currentQuestionNumber = 0;
 
   while (correctAnswers < maxQuestions) {
-
     const correctAnswer = game();
     const userAnswer = readlineSync.question('Your answer: ');
 
