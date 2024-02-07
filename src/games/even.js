8599c3f5bcf9d537473747be2baef8612e603f77
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-02-07 22:54:28 +0300

    linter

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,8 @@
 export const playEven = () => {
-const number = Math.floor(Math.random() * 100);
-console.log(`Question: ${number}`);
- if (number % 2 === 0) {
-  return 'yes';
- } else {
-  return 'no';
+  const number = Math.floor(Math.random() * 100);
+  console.log(`Question: ${number}`);
+  if (number % 2 === 0) {
+    return 'yes';
   }
- };
+    return 'no';
+};
