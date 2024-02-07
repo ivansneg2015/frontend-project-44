9d9073aee88ad28453e365eb540b4762a5fb9ff0
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-02-07 13:02:27 +0300

    linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 import greetUser from '../src/cli.js';
 
-console.log(`Welcome to the Brain Games!`);
-greetUser();
+console.log('Welcome to the Brain Games!');
+greetUser();#!/usr/bin/env node
+import { playPrime } from '../src/games/prime.js';
+
+import { playGame } from '../src/index.js';
+
+const question = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+playGame(playPrime, question);
