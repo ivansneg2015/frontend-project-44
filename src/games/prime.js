9ebc9a6afd8be3ac8cc9ac84661c1c432b38df5d
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-02-07 23:12:10 +0300

    linter

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,18 +2,18 @@ const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  for (let i = 2; i <= Math.sqrt(num)) {
     if (num % i === 0) {
       return false;
     }
   }
 
   return true;
-  };
+};
 
-  export const playPrime = () => {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${randomNumber}`);
-    const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
-    return correctAnswer;
-  };
+export const playPrime = () => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(`Question: ${randomNumber}`);
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+  return correctAnswer;
+};
